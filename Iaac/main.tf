3fa7f4eca8b4e85a33612edf43ac8473fbe134aa
yogeshree <yogeshreec@gmail.com>
2025-08-12 12:33:02 +0530

    main teraform

--- Iaac/main.tf
+++ Iaac/main.tf
@@ -1,1 +1,67 @@
+resource "aws_s3_bucket" "tf-script-bucket-yc" {
+  bucket = var.tf-script-bucket-yc
+}
 
+
+resource "aws_s3_bucket" "tf-parquet-bucket-yc" {
+  bucket = var.tf-parquet-bucket-yc
+}
+
+
+resource "aws_s3_bucket" "tf-cleaned-bucket-yc" {
+  bucket = var.tf-cleaned-bucket-yc
+}
+
+resource "aws_glue_catalog_database" "tf_crawler_db" {
+  name = "tf_crawler_db"
+}
+
+
+
+locals {
+  glue_role_arn = "arn:aws:iam::968164097585:role/LabRole"
+}
+
+
+resource "aws_glue_job" "tf_ingestion_glue_job" {
+  name     = var.tf_ingestion_glue_job
+  role_arn = local.glue_role_arn
+
+  command {
+    name            = "glueetl"
+    script_location = var.script_ingestion_path
+    python_version  = "3"
+  }
+
+  glue_version      = "4.0"
+  number_of_workers = 2
+  worker_type       = "G.1X"
+}
+
+resource "aws_glue_job" "tf_transformation_glue_job" {
+  name     = var.tf_transformation_glue_job
+  role_arn = local.glue_role_arn
+
+  command {
+    name            = "glueetl"
+    script_location = var.script_transformation_path
+    python_version  = "3"
+  }
+
+  glue_version      = "4.0"
+  number_of_workers = 2
+  worker_type       = "G.1X"
+}
+resource "aws_glue_crawler" "tf_glue_crawler_name" {
+  name          = var.glue_crawler_name
+  role          = local.glue_role_arn
+  database_name = aws_glue_catalog_database.tf_crawler_db.name
+
+  s3_target {
+    path = "s3://${aws_s3_bucket.tf-cleaned-bucket-yc.bucket}/final_master/"
+  }
+  depends_on = [
+    aws_glue_job.tf_ingestion_glue_job,
+    aws_glue_job.tf_transformation_glue_job
+  ]
+}
